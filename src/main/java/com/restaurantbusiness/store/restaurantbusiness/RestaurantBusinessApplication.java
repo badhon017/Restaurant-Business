bdaaa05759172ad262257c7e64c0f2246d4fe0eb
badhon017 <badhonbrinta25@gmail.com>
2024-02-01 20:28:40 +0600

    restaurant business db setup and project bootstrap

---
+++ src/main/java/com/restaurantbusiness/store/restaurantbusiness/RestaurantBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.restaurantbusiness.store.restaurantbusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantBusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantBusinessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restaurantbusiness/store/restaurantbusiness/RestaurantBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurantbusiness.store.restaurantbusiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantBusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
